994fd10aa0c084122c0bc0098425a9bcf0ef6344
Seth Baxter <seth.baxter@dexcom.com>
2019-03-19 11:21:13 -0400

    Add leiningen profiles to scm

---
+++ profiles.clj
@@ -0,0 +1,11 @@
+{:user {:aliases      {"integration" ["do" "clean" ["uberjar"] ["with-profile" "+integration" "test"]]
+                       "lint"        ["do" "clean" ["kibit"] ["eastwood"] ["bikeshed"]]
+                       "proj"        ["do" "clean" ["deps" ":tree"] ["ancient"]]
+                       "uber"        ["do" "clean" ["deps" ":tree"] ["ancient"] ["kibit"] ["eastwood"] ["bikeshed"]]}
+        :dependencies [[nrepl "0.6.0"]]
+        :plugins      [[cider/cider-nrepl "0.21.1"]
+                       [jonase/eastwood "0.3.5"]
+                       [lein-ancient "0.6.15"]
+                       [lein-bikeshed "0.5.1"]
+                       [lein-kibit "0.1.6"]
+                       [com.roomkey/lein-v "7.0.0"]]}}
