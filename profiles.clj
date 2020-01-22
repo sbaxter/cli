9a5395e8fa5572ef215ea126b674ffeb7c757a24
Seth Baxter <seth.baxter@dexcom.com>
2020-01-22 11:20:41 -0500

    Bump lein deps

--- profiles.clj
+++ profiles.clj
@@ -4,12 +4,12 @@
                            "omni"        ["do" "clean" ["deps" ":tree"] ["ancient"] ["kibit"] ["eastwood"] ["bikeshed"]]}
             :dependencies [[nrepl "0.6.0"]]
             :jvm-opts     ["-Dapple.awt.UIElement=true"]
-            :plugins      [[cider/cider-nrepl "0.21.1"]
-                           [jonase/eastwood "0.3.5"]
+            :plugins      [[cider/cider-nrepl "0.23.0"]
+                           [jonase/eastwood "0.3.7"]
                            [lein-ancient "0.6.15"]
                            [lein-bikeshed "0.5.2"]
                            [lein-difftest "2.0.0"]
-                           [lein-kibit "0.1.6"]
+                           [lein-kibit "0.1.8"]
                            [lein-pprint "1.2.0"]
                            [lein-try "0.4.3"]
                            [com.roomkey/lein-v "7.1.0"]]}
